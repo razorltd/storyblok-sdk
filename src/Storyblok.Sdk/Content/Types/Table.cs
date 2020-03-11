026389546101ed489b5375b32e00ed799a3fc3cf
Laura Smythe <laura.smythe@razor.co.uk>
2020-03-11 09:27:24 +0000

    Add table type

---
+++ src/Storyblok.Sdk/Content/Types/Table.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Storyblok.Sdk.Content.Types
+{
+    public class Table
+    {
+        [JsonProperty(PropertyName = "tbody")]
+        public IEnumerable<TableRow> TableBody { get; set; }
+
+        [JsonProperty(PropertyName = "thead")]
+        public IEnumerable<TableHeader> TableHead { get; set; }
+    }
+
+    public class TableHeader
+    {
+        public string Value { get; set; }
+    }
+
+    public class TableRow : IContent
+    {
+        public IEnumerable<TableColumn> Body { get; set; }
+    }
+
+    public class TableColumn : IContent
+    {
+        public string Value { get; set; }
+    }
+}
