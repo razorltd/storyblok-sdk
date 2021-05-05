0a18131122a07104d238abcd794fddc47dcd253a
Laura Smythe <laura.smythe@razor.co.uk>
2021-05-05 10:49:15 +0100

    update dependencies

--- src/Storyblok.Sdk/IStoryblokClient.cs
+++ src/Storyblok.Sdk/IStoryblokClient.cs
@@ -19,27 +19,32 @@ namespace Storyblok.Sdk
             [Query] [AliasAs("with_tag")] string withTag,
             [Query] [AliasAs("per_page")] int? perPage = 100,
             [Query] [AliasAs("page")]int? page = 1,
-            [Query] [AliasAs("sort_by")]string sortBy = "first_published_at:desc");
+            [Query] [AliasAs("sort_by")]string sortBy = "first_published_at:desc",
+            [Query] [AliasAs("language")]string language = "en");
 
         [Get("/stories")]
         Task<MultipleStoryResponse<T>> FilterStoriesWithFullTextSearchAsync<T>(
             [Query] [AliasAs("starts_with")] string startsWith,
-            [Query] [AliasAs("search_term")] string searchTerm);
+            [Query] [AliasAs("search_term")] string searchTerm,
+            [Query][AliasAs("language")] string language = "en");
 
         [Get("/stories")]
         Task<HttpResponseMessage> GetStories<T>(
             [Query] [AliasAs("starts_with")] string startsWith,
             [Query] [AliasAs("per_page")]int? perPage = 100,
             [Query] [AliasAs("page")]int? page = 1,
-            [Query] [AliasAs("sort_by")]string sortBy = "first_published_at:desc");
+            [Query] [AliasAs("sort_by")]string sortBy = "first_published_at:desc",
+            [Query][AliasAs("language")] string language = "en");
 
         [Get("/stories")]
         Task<HttpResponseMessage> GetAllStories<T>(
             [Query] [AliasAs("per_page")]int? perPage = 100,
             [Query] [AliasAs("page")]int? page = 1,
-            [Query] [AliasAs("sort_by")]string sortBy = "first_published_at:desc");
+            [Query] [AliasAs("sort_by")]string sortBy = "first_published_at:desc",
+            [Query][AliasAs("language")] string language = "en");
 
         [Get("/stories/{fullSlug}")]
-        Task<SingleStoryResponse<T>> GetStory<T>(string fullSlug);
+        Task<SingleStoryResponse<T>> GetStory<T>(string fullSlug,
+            [Query][AliasAs("language")] string language = "en");
     }
 }
